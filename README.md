82d8e905fe0f575594849daa5fe4e4989b869d81
Rémy's Lab <remyslab@users.noreply.github.com>
2017-04-06 04:18:28 +0200

    punnett -> Latextablemaker (#1)
    * main latex table, \begin{tabular}{} is missing, need to improve frequencies display
    
    * some mistakes fixed, freq table added
    
    * Ready to use with python2
    
    * Update README and some screenshoots
    
    * Silly mistakes fixed
    
    * Silly mistake again...
    
    * Last last..?
    
    * Add a python3-compatible version of the punnettsquaremaker
    
    * Latex as an option, less open/close the file
    
    * little fix
    
    * Add the new version for Python3

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
-PunnetSquareMaker
-=================
+PunnettSquareMaker
+============================
 
-A Python 2.6 program which creates Punnett squares and calculates potential offspring genotype frequencies.
+A Python2 / Python3 program which creates Punnett squares and calculates potential offspring genotype frequencies.
 
-To use, enter genotypes of the parents when prompted. There should be two alleles per gene (each represented by a single character), and genes should be separated by spaces.
-For example, "Xx Yy ZZ" and "XX YY zz" are valid input strings, while "XxYyZZ" is not. 
+![Punnett](https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Punnett_square_mendel_flowers.svg/240px-Punnett_square_mendel_flowers.svg.png)
+
+*Schema : Madeleine Price Ball*
+
+[More about the Punnett square on Wikipedia.](https://en.wikipedia.org/wiki/Punnett_square)
+
+To use, enter genotypes of the parents when prompted. There should be **two alleles per gene** (each represented by a single character), and **genes should be separated by spaces**.
+
+For example, "Xx Yy ZZ" and "XX YY zz" are valid input strings, while "XxYyZZ" or ""Xx Yy ZZ " *(notice the space at the end...)* is not. 
 
 The program will then print a formatted Punnett square, and list the genotype frequencies for each distinct resulting genotype.
+
+![Img](screenshot1.png)
+
+# Latex features
+This script generates two Latex tables, the Punnett square and the frequencies table. 
+
+![Img](screenshot2.png)
+
+
--- punnetsquaremaker_python3.py
+++ punnetsquaremaker_python3.py
@@ -61,25 +61,25 @@ def make_table(parent1, parent2):
 def print_table(table, c1, c2): # formats and prints Punnett square
 	latextable = []
 	divlength = (len(c1[0])*2+4)*2**(len(c1[0]))
-	print ''
-	print '',
+	print('')
+	print('', end=' ')
 	for a in c2:
-		print ' '*(len(c1[0])+3) + a + '',
+		print(' '*(len(c1[0])+3) + a + '', end=' ')
 		latextable.append('& ' + a + ' ')
-	print '\n' + ' '*(len(c1[0])+1) + '-'*(divlength)
+	print('\n' + ' '*(len(c1[0])+1) + '-'*(divlength))
 	latextable.append('\\\ \n\\hline\n')
 	
 	for i, row in enumerate(table):
-		print c1[table.index(row)],
+		print(c1[table.index(row)], end=' ')
 		latextable.append(c1[table.index(row)] + ' & ')
-		print '|',
+		print('|', end=' ')
 		for j, cell in enumerate(row):
-			print cell + ' | ',
+			print(cell + ' | ', end=' ')
 			if j != len(row)-1:
 				latextable.append(cell + ' & ')
 			else:
 				latextable.append(cell + ' ')
-		print '\n' + ' '*(len(c1[0])+1) + '-'*(divlength)
+		print('\n' + ' '*(len(c1[0])+1) + '-'*(divlength))
 		if i != len(table)-1:
 			latextable.append('\\\ \n')	
 	return latextable		
@@ -95,29 +95,29 @@ def print_genotype_frequencies(table): # calculates frequencies for each genotyp
 			if sorted(x) == sorted(y):
 				count += 1
 		if sorted(x) not in calculated:
-			print "The frequency of the " + x + " genotype is " + str(float(count)/float((len(genotypes)))*100) + "%."
+			print("The frequency of the " + x + " genotype is " + str(float(count)/float((len(genotypes)))*100) + "%.")
 			freqtable.append(x + ' & ' + str(float(count)/float((len(genotypes)))*100) + '\\% \\\ \\hline \n')	
 		calculated.append(sorted(x))
 	return freqtable	
 
-print '' 
-print '==========   Punnett square maker & Latex table export  =============='
-print '' 
-print 'Hello, and welcome to the Punnett square maker! To get started, enter the genotypes of each parent. There should be two alleles for each gene, and each should be represented by one letter.'
-print 'The genes should be separated by spaces. For example, a valid genotype would be "Xx Yy zz", while "XxYyZz" or "Xx Yy zz " would not.'
-print '' 
-print '====================================================================='
-print ''
+print('') 
+print('==========   Punnett square maker & Latex table export  ==============')
+print('') 
+print('Hello, and welcome to the Punnett square maker! To get started, enter the genotypes of each parent. There should be two alleles for each gene, and each should be represented by one letter.')
+print('The genes should be separated by spaces. For example, a valid genotype would be "Xx Yy zz", while "XxYyZz" or "Xx Yy zz " would not.')
+print('') 
+print('=====================================================================')
+print('')
 while True:
-	p1 = raw_input("Please enter the genotype of the first parent: ").split(' ')
-	p2 = raw_input("Please enter the gentype of the second parent: ").split(' ')
+	p1 = input("Please enter the genotype of the first parent: ").split(' ')
+	p2 = input("Please enter the gentype of the second parent: ").split(' ')
 	c1 = get_all_combinations(p1)
 	c2 = get_all_combinations(p2)
 	a = make_table(c1, c2)
 	latextable = print_table(a, c1, c2)
 	freqtable = print_genotype_frequencies(a)
-	print ''
-	option = raw_input("Enter (S) to save your tables in a *.tex file, or any key to continue without saving.\n")
+	print('')
+	option = input("Enter (S) to save your tables in a *.tex file, or any key to continue without saving.\n")
 	if option == "S":
 		now = datetime.datetime.now() # Date & time
 		name = '%s_table.tex' % (now.strftime("%Y-%m-%d_%H-%M-%S")) # Give a name with date
@@ -132,11 +132,11 @@ while True:
 			f.write("%s" % item)
 		f.write(foottex())
 		f.close()
-		print '' 
-		print 'Your Latex file ' + name + ' is saved in your script repertory!\n'
-	action = raw_input("Enter (A) to make another or any key to quit!\n")
+		print('') 
+		print('Your Latex file ' + name + ' is saved in your script repertory!\n')
+	action = input("Enter (A) to make another or any key to quit!\n")
 	if action == "A":
-		print ''
-		print "Again!\n"
+		print('')
+		print("Again!\n")
 	else:	
 		quit()
